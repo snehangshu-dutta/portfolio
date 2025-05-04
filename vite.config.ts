5f80971c2a420ba4aff5b0ac7d7bd510646f1704
snehangshu-dutta <jobs.snehangshu@gmail.com>
2025-05-04 23:02:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio/', // IMPORTANT: this must match your repo name exactly
+  base: '/portfolio/', 
   plugins: [react()],
 })
